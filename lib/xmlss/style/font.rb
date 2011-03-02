62a43802bbc5da9da2e028f356723b5e571b431a
Kelly <kelly@kelredd.com>
2011-03-01 18:04:14 -0600

    tests and classes for Style::Font

---
+++ lib/xmlss/style/font.rb
@@ -0,0 +1,66 @@
+module Xmlss::Style
+  class Font
+
+    UNDERLINES = {
+      :none => 0,
+      :single => 1,
+      :double => 2,
+      :default => 3
+    }.freeze
+
+    ALIGNMENTS = {
+      :none => 0,
+      :subscript => 1,
+      :superscript => 2,
+      :default => 3
+    }.freeze
+
+    class << self
+      def underline(u)
+        UNDERLINES[u.to_sym]
+      end
+      def alignment(a)
+        ALIGNMENTS[a.to_sym]
+      end
+    end
+
+    attr_accessor :bold, :color, :name, :italic, :size, :strike_through
+    attr_accessor :underline, :alignment
+
+    def initialize(attrs={})
+      self.bold = attrs[:bold] || false
+      self.color = attrs[:color]
+      self.name = attrs[:name]
+      self.italic = attrs[:italic] || false
+      self.size = attrs[:size]
+      self.strike_through = attrs[:strike_through] || false
+      self.underline = attrs[:underline] || :default
+      self.alignment = attrs[:alignment] || :default
+    end
+
+    def bold?; !!self.bold; end
+    def italic?; !!self.italic; end
+    def strike_through?; !!self.strike_through; end
+
+    def underline=(value)
+      @underline = if value && UNDERLINES.has_key?(value.to_sym)
+        UNDERLINES[value.to_sym]
+      elsif UNDERLINES.has_value?(value)
+        value
+      else
+        nil
+      end
+    end
+
+    def alignment=(value)
+      @alignment = if value && ALIGNMENTS.has_key?(value.to_sym)
+        ALIGNMENTS[value.to_sym]
+      elsif ALIGNMENTS.has_value?(value)
+        value
+      else
+        nil
+      end
+    end
+
+  end
+end
---
+++ test/style/font_test.rb
@@ -0,0 +1,99 @@
+require "test/helper"
+require 'xmlss/style/font'
+
+class Xmlss::Style::FontTest < Test::Unit::TestCase
+
+  context "Xmlss::Style::Font" do
+    subject { Xmlss::Style::Font.new }
+
+    should_have_class_method :underline
+    {
+      :none => 0,
+      :single => 1,
+      :double => 2,
+      :default => 3
+    }.each do |underline, value|
+      should "provide the value for the '#{underline}' underline" do
+        assert_equal value, Xmlss::Style::Font.underline(underline)
+      end
+    end
+
+    should_have_class_method :alignment
+    {
+      :none => 0,
+      :subscript => 1,
+      :superscript => 2,
+      :default => 3
+    }.each do |alignment, value|
+      should "provide the value for the '#{alignment}' alignment" do
+        assert_equal value, Xmlss::Style::Font.alignment(alignment)
+      end
+    end
+
+    should_have_accessors :bold, :color, :name, :italic, :size, :strike_through
+    should_have_accessors :underline, :alignment
+    should_have_instance_methods :bold?, :italic?, :strike_through?
+
+    should "set it's defaults" do
+      assert_equal false, subject.bold
+      assert_equal nil, subject.color
+      assert_equal nil, subject.name
+      assert_equal false, subject.italic
+      assert_equal nil, subject.size
+      assert_equal false, subject.strike_through
+      assert_equal Xmlss::Style::Font.underline(:default), subject.underline
+      assert_equal Xmlss::Style::Font.alignment(:default), subject.alignment
+    end
+
+    context "that sets attributes at init" do
+      before do
+        @attrs = {
+          :bold => true,
+          :color => '#FF0000',
+          :name => "Verdana",
+          :italic => true,
+          :size => 10,
+          :strike_through => true,
+          :underline => :single,
+          :alignment => :superscript
+        }
+        @font = Xmlss::Style::Font.new(@attrs)
+      end
+      subject{ @font }
+
+      should "should set them correctly" do
+        @attrs.reject{|a, v| [:underline, :alignment].include?(a)}.each do |a,v|
+          assert_equal v, subject.send(a)
+        end
+        assert_equal Xmlss::Style::Font.underline(:single), subject.underline
+        assert_equal Xmlss::Style::Font.alignment(:superscript), subject.alignment
+      end
+    end
+
+    context "that sets underline and alignment by key" do
+      before do
+        subject.underline = :double
+        subject.alignment = :subscript
+      end
+
+      should "should returm it by value" do
+        assert_equal Xmlss::Style::Font.underline(:double), subject.underline
+        assert_equal Xmlss::Style::Font.alignment(:subscript), subject.alignment
+      end
+    end
+
+    context "that sets underline and alignment by value" do
+      before do
+        subject.underline = Xmlss::Style::Font.underline(:double)
+        subject.alignment = Xmlss::Style::Font.alignment(:subscript)
+      end
+
+      should "should returm it by value" do
+        assert_equal Xmlss::Style::Font.underline(:double), subject.underline
+        assert_equal Xmlss::Style::Font.alignment(:subscript), subject.alignment
+      end
+    end
+
+  end
+
+end
