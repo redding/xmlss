71a47aded3482296e4af80daa4d430b9f2d875c4
Kelly <kelly@kelredd.com>
2011-03-04 15:20:54 -0600

    added some common xml generation tests

--- test/helper.rb
+++ test/helper.rb
@@ -2,4 +2,31 @@ require 'rubygems'
 require 'test_belt'
 require 'test/env'
 
-# TODO: setup your test environment here
+class Test::Unit::TestCase
+
+  class << self
+
+    def should_build_node
+      should_have_instance_methods :build_node
+      should "build it's node" do
+        assert_nothing_raised do
+          ::Nokogiri::XML::Builder.new do |builder|
+            subject.build_node(builder)
+          end
+        end
+      end
+    end
+
+    def should_build_no_attributes_by_default(klass)
+      context "by default" do
+        subject{ klass.new }
+
+        should "have no element attributes" do
+          assert_equal({}, subject.send(:build_attributes))
+        end
+      end
+    end
+
+  end
+
+end
