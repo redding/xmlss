953d948b95c8e08d291ba26f99e7be57232aa709
Kelly <kelly@kelredd.com>
2011-12-21 10:14:35 -0600

    updated examples for new api

--- examples/text.rb
+++ examples/text.rb
@@ -1,36 +1,26 @@
 require 'examples/example_workbook'
 
-class Text < ExampleWorkbook
-  def name; "text"; end
-  def build
+# $ bundle exec ruby examples/text.rb
 
-    # add in general text style
-    self.styles << Xmlss::Style::Base.new('general_text') do
-      alignment(
-        :horizontal => :left,
-        :vertical => :top,
-        :wrap_text => true
-      )
-      number_format(
-        :format => "@"  # set format to text explicitly
-      )
-    end
+ExampleWorkbook.new("text") do
 
-    # add single cell with text data and specify general text style
-    wksht = Xmlss::Worksheet.new('text')
-    wksht.table.columns << Xmlss::Column.new
-    row1 = Xmlss::Row.new
-    row1.cells << Xmlss::Cell.new({
-      :style_id => "general_text",
-      :data => Xmlss::Data.new(%{
+  style 'general_text' do
+    alignment(:horizontal => :left, :vertical => :top, :wrap_text => true)
+    # set format to text explicitly
+    number_format(:format => "@")
+  end
+
+  worksheet('text') do
+    column
+
+    row {
+      cell(:style_id => "general_text") {
+        data %{
 A blob of text
 with line breaks
-  and leading space
-      })
-    })
-    wksht.table.rows << row1
-    self.worksheets << wksht
-
+  and leading space}
+      }
+    }
   end
+
 end
-Text.new.to_file(:format)
