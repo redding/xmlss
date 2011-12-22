04d4e9a54001001bea50bc4f77d3fcec10f0d96c
Kelly <kelly@kelredd.com>
2011-12-22 11:58:27 -0600

    added rake tasks for running the examples, profiler, and running tests together with those

---
+++ bench/profiler_runner.rb
@@ -0,0 +1,43 @@
+require 'ruby-prof'
+require 'xmlss'
+
+class XmlssProfilerRunner
+
+  attr_reader :result
+
+  def initialize(n)
+    build = Proc.new do
+      worksheet("5 columns, #{n} rows") {
+        column
+        column
+        column
+        column
+        column
+
+        n.times do |i|
+          row {
+            # put data into the row (infer type)
+            [1, "text", 123.45, "0001267", "$45.23"].each do |data_value|
+              cell { data data_value }
+            end
+          }
+        end
+      }
+    end
+
+    @result = RubyProf.profile do
+      Xmlss::Workbook.new(:output => {:pp => 2}, &build).to_file("./bench/profiler_#{n}.xml")
+    end
+
+  end
+
+  def print_flat(outstream, opts={})
+    RubyProf::FlatPrinter.new(@result).print(outstream, opts)
+    #RubyProf::GraphPrinter.new(@result).print(outstream, opts)
+  end
+
+  def print_graph(outstream, opts={})
+    RubyProf::GraphPrinter.new(@result).print(outstream, opts)
+  end
+
+end
